5c2b83e5a64639b294d13818cf53678dbf327d3a
hamzadar2013 <hamza.dar@camp1.tkxel.com>
2025-03-22 03:17:31 -0500

    code push integrated

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
 import React, { useEffect } from 'react';
 import {
   AppState,
+  Button,
   SafeAreaView,
+  Text,
 
 } from 'react-native';
-
-
+import messaging from '@react-native-firebase/messaging';
 import codePush from "react-native-code-push";
 import CodePush from "react-native-code-push";
-
+import {
+  checkNotifications,
+  requestNotifications,
+} from "react-native-permissions";
 
 let codePushOptions = {
   checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
@@ -27,10 +31,59 @@ let codePushOptions = {
 function App(): React.JSX.Element {
 
 
+  const checkNotificationPermission = async () => {
+    if (true) {
+      checkNotifications().then(({ status, settings }) => {
+        if (status == "granted") {
+          messagingRequest();
+        } else if (status == "denied") {
+          requestNotifications(["alert", "sound"]).then(
+            ({ status, settings }) => {
+              if (status == "granted") {
+                messagingRequest();
+              }
+            }
+          );
+        }
+      });
+    } else {
+      messagingRequest();
+    }
+  };
+  async function messagingRequest() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
+
+  const getToke=async ()=>{
+    try {
+    
+      const token = await messaging().getToken()
+      console.log('FCM Token:', token); 
+    } catch (error) {
+      console.log("error:", error);
+      
+    }
+ 
+
+    
+  }
+
+  useEffect(()=>{
+    checkNotificationPermission();
+getToke()
+  },[])
  
   return (
-    <SafeAreaView style={{backgroundColor:"blue",flex:1}}>
-   
+    <SafeAreaView style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+   <Text>Hamza Dar Code Push Test</Text>
+   <Button  onPress={()=>{}} title='Click me'/>
     </SafeAreaView>
   );
 }
